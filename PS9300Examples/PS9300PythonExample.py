f6fbd5adbfe68fe171c0af8d8478cb6d8d5fdc2a
hitesh.mistry <hsmistry@users.noreply.github.com>
2018-06-04 17:00:43 +0100

    Update formatting

--- PS9300Examples/PS9300PythonExample.py
+++ PS9300Examples/PS9300PythonExample.py
@@ -15,7 +15,7 @@ import time
 COMRCW = win32com.client.Dispatch("PicoSample3.COMRC") # create COM object
 COMRCW.ExecCommand("Gui:Control:Invisible")
 
-#set up measurements
+# Set up measurements
 COMRCW.ExecCommand("Meas:Display:Param")
 COMRCW.ExecCommand("Meas:DisplSrc:Ch1")
 COMRCW.ExecCommand("Meas:Mode:Single")
@@ -26,19 +26,17 @@ COMRCW.ExecCommand("Meas:Ch1:YParam:Min 1")
 COMRCW.ExecCommand("Meas:Ch1:YParam:PP 1")
 
 
-COMRCW.ExecCommand("*RunControl:Single")  #set running mode of scope
+COMRCW.ExecCommand("*RunControl:Single")  # Set running mode of scope
 time.sleep(2)
-COMRCW.ExecCommand("Header Off") # turn off headers for returned values
+COMRCW.ExecCommand("Header Off") # Turn off headers for returned values
 
-strdata = COMRCW.ExecCommand("Wfm:Data?") #get wfm data for ch1
-YU = COMRCW.ExecCommand("Wfm:Preamb:YU?") #get y scale units
-XU = COMRCW.ExecCommand("Wfm:Preamb:XU?") #get x scale units
-XInc = COMRCW.ExecCommand("Wfm:Preamb:XInc?") #get time sample interval
-XOrg = COMRCW.ExecCommand("Wfm:Preamb:XOrg?") #get y axis origin
+strdata = COMRCW.ExecCommand("Wfm:Data?")       # Get wfm data for ch1
+YU = COMRCW.ExecCommand("Wfm:Preamb:YU?")       # Get y scale units
+XU = COMRCW.ExecCommand("Wfm:Preamb:XU?")       # Get x scale units
+XInc = COMRCW.ExecCommand("Wfm:Preamb:XInc?")   # Get time sample interval
+XOrg = COMRCW.ExecCommand("Wfm:Preamb:XOrg?")   # Get y axis origin
 
-#Measurements
-#List1 = COMRCW.ExecCommand("Meas:Res:List?")
-#print(List1)
+# Measurements
 freq = COMRCW.ExecCommand("Meas:Res:1?")
 print("Frequency = " + str(freq))
 riseTime = COMRCW.ExecCommand("Meas:Res:2?")
@@ -50,19 +48,20 @@ print("Minimum = " + str(minimum))
 Pp = COMRCW.ExecCommand("Meas:Res:5?")
 print("Peak-to-Peak = " + str(Pp))
 
-#convert data to array of floats
+# Convert data to array of floats
 strdata = str(strdata)
 strdata = strdata.split(',')
 strdata = np.asarray(strdata)
 data = strdata.astype(np.float)
 
-#create time data array
+# Create time data array
 totaltime = len(data) * float(XInc)
 totaltime = float(totaltime)
 datatime = np.linspace(0, totaltime, len(data))
 
-#plot data
+# Plot data
 plt.plot(datatime, data)
+plt.title('PicoScope 9300 Series Example')
 plt.xlabel('Time (' + XU +')')
 plt.ylabel('Voltage (' + YU +')')
 plt.show()
